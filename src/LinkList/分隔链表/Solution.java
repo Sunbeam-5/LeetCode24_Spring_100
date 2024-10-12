db368f6e39a3eeef0db9bd04cb1702ec45e02cb5
Sunbeam-5 <90610326+Sunbeam-5@users.noreply.github.com>
2024-10-12 20:31:08 +0800

    日常练习

--- src/LinkList/分隔链表/Solution.java
+++ src/LinkList/分隔链表/Solution.java
@@ -4,28 +4,31 @@ package LinkList.分隔链表;
 
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        int index = 0;
-        ListNode cur = head;
-        ListNode list = new ListNode(0);
+        // 创建两个链表：一个保存小于 x 的节点，另一个保存大于等于 x 的节点
+        ListNode before = new ListNode(0);
+        ListNode after = new ListNode(0);
 
-        // System.out.println(index);
-        // return list;
-        cur = head;
+        // 保留 before 和 after 链表的头节点，最后方便返回结果
+        ListNode beforeHead = before;
+        ListNode afterHead = after;
+
+        ListNode cur = head;
+
+        // 遍历原链表
         while (cur != null) {
             if (cur.val < x) {
-                list.val = cur.val;
-                list = list.next;
+                before.next = cur;  // 添加到小于 x 的链表
+                before = before.next;
+            } else {
+                after.next = cur;   // 添加到大于等于 x 的链表
+                after = after.next;
             }
             cur = cur.next;
         }
-        cur = head;
-        while (cur != null) {
-            if (cur.val >= x) {
-                list.val = cur.val;
-                list = list.next;
-            }
-            cur = cur.next;
-        }
-        return list;
+
+        after.next = null;  // 避免链表环
+        before.next = afterHead.next;  // 连接两个链表
+
+        return beforeHead.next;  // 返回新链表的头节点
     }
 }
